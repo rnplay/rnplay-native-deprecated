0f170923b98a9a8c18b534dbdee07b7abfd02cd0
Christopher <hello@blick-labs.com>
2015-05-25 22:42:58 -0700

    add navigation bar component

---
+++ App/Components/NavigationBar.js
@@ -0,0 +1,99 @@
+'use strict';
+
+var React = require('react-native');
+var Dimensions = require('Dimensions');
+
+var {
+  PixelRatio,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View
+} = React;
+
+var NavigationBar = React.createClass({
+
+  getTitle() {
+   return (<Text style={styles.navTitle}>{this.props.title}</Text>); 
+  },
+
+  getLeftButton() {
+    var {
+      onPrev,
+      prevTitle
+    } = this.props;
+
+    if(this.props.onPrev && this.props.prevTitle) {
+      return (
+        <TouchableOpacity onPress={onPrev}>
+          <Text style={styles.navButton}>
+            {prevTitle}
+          </Text>
+        </TouchableOpacity>
+      )
+    } else {
+      return (
+        <TouchableWithoutFeedback>
+          <Text style={styles.navButton}></Text>
+        </TouchableWithoutFeedback>
+      );
+    }
+  },
+
+  getRightButton() {
+    var {
+      onNext,
+      nextTitle,
+    } = this.props;
+
+    if(this.props.onNext && this.props.nextTitle) {
+      return (
+        <TouchableOpacity onPress={onPrev}>
+          <Text style={styles.navButton}>
+            {prevTitle}
+          </Text>
+        </TouchableOpacity>
+      )
+    } else {
+      return (
+        <TouchableWithoutFeedback>
+          <Text style={styles.navButton}></Text>
+        </TouchableWithoutFeedback>
+      );
+    }
+  },
+
+  render() {
+    return (
+      <View style={styles.navbarContainer}>
+        {this.getLeftButton()}
+        {this.getTitle()}
+        {this.getRightButton()}
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  navbarContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderBottomColor: 'rgba(0, 0, 0, 0.5)',
+    borderBottomWidth: 1 / PixelRatio.get(),
+    justifyContent: 'space-between',
+    padding: 10
+  },
+  navButton: {
+    flex: 0.1,
+    fontSize: 15,
+  },
+  navTitle: {
+    flex: 0.8,
+    textAlign: 'center',
+    fontSize: 15,
+  },
+});
+
+module.exports = NavigationBar;
