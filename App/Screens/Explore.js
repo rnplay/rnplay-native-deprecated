b538306861ef1c3d864af0adb5c4f1b9d48c7791
Dave Sibiski <dsibiski@gmail.com>
2015-05-30 23:29:31 -0400

    Adds a 'Help' button to the right of the SegmentedControl
    - 'About' screen rendered in the modal (not much content right now)

--- App/Screens/Explore.js
+++ App/Screens/Explore.js
@@ -8,11 +8,19 @@ var {
   Text,
   View,
   StatusBarIOS,
+  TouchableOpacity,
 } = React;
 
 var AppList = require("../Components/AppList");
+var About = require('../Screens/About');
+var Icon = require('FAKIconImage');
+
+var Modal = require('react-native-modal');
+var DeviceHeight = require('Dimensions').get('window').height;
 
 var Explore = React.createClass({
+  mixins: [Modal.Mixin],
+
   getInitialState() {
     return {
       values: ['Picks', 'Search'],
@@ -47,19 +55,48 @@ var Explore = React.createClass({
     });
   },
 
+  showModalTransition(transition) {
+    transition('opacity', {duration: 200, begin: 0, end: 1});
+    transition('height', {duration: 200, begin: DeviceHeight * 2, end: DeviceHeight});
+  },
+
+  hideModalTransition(transition) {
+    transition('height', {duration: 200, begin: DeviceHeight, end: DeviceHeight * 2, reset: true});
+    transition('opacity', {duration: 200, begin: 1, end: 0});
+  },
+
   render(){
     StatusBarIOS.setStyle(StatusBarIOS.Style.darkContent);
     return (
       <View style={styles.mainContainer}>
-        <View style={styles.segmentContainer}>
-          <SegmentedControlIOS
-          tintColor={'#712FA9'}
-          values={this.state.values}
-          selectedIndex={this.state.selectedIndex}
-          onValueChange={this._onValueChange} />
+        <View style={styles.topContainer}>
+          <View style={styles.segmentContainer}>
+            <SegmentedControlIOS
+              tintColor={'#712FA9'}
+              values={this.state.values}
+              selectedIndex={this.state.selectedIndex}
+              onValueChange={this._onValueChange} />
+          </View>
+          <TouchableOpacity onPress={() => this.openModal()} style={styles.infoButton}>
+            <Icon
+              name='ion|ios-help'
+              size={25}
+              color='#712FA9'
+              style={styles.infoIcon} />
+          </TouchableOpacity>
         </View>
         {this.state.value == 'Picks' && this.renderPicks()}
         {this.state.value == 'Search' && this.renderSearch()}
+        <Modal
+          backdropType="blur"
+          backdropBlur="dark"
+          isVisible={this.state.isModalOpen}
+          forceToFront={true}
+          customShowHandler={this.showModalTransition}
+          customHideHandler={this.hideModalTransition}
+          onPressBackdrop={() => this.closeModal()}>
+          <About />
+        </Modal>
       </View>
     )
   }
@@ -72,8 +109,23 @@ var styles = StyleSheet.create({
     flexDirection: 'column',
     backgroundColor: 'white'
   },
+  topContainer: {
+    flexDirection: 'row'
+  },
   segmentContainer: {
+    flex: 2,
     margin: 20,
+    marginRight: 0,
+  },
+  infoButton: {
+    flex: 1,
+  },
+  infoIcon: {
+    alignSelf: 'center',
+    width: 35,
+    height: 35,
+    marginLeft: 10,
+    marginRight: 10,
   },
 });
 
