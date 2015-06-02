a8f79d2930456e55a393761316e296c0e2ebd67c
Dave Sibiski <dsibiski@gmail.com>
2015-06-02 18:59:51 -0400

    Moves 'Help' to it's own Tab/Screen
    - Renames 'About' screen to 'Help'
    - Removes 'Accordions' on 'Help' screen since we have plenty of room on that screen now

--- App/Screens/Explore.js
+++ App/Screens/Explore.js
@@ -12,17 +12,9 @@ var {
 } = React;
 
 var AppList = require("../Components/AppList");
-var About = require('../Screens/About');
 var Search = require('../Screens/Search');
 
-var Icon = require('FAKIconImage');
-
-var Modal = require('react-native-modal');
-var DeviceHeight = require('Dimensions').get('window').height;
-
 var Explore = React.createClass({
-  mixins: [Modal.Mixin],
-
   getInitialState() {
     return {
       values: ['Picks', 'Search'],
@@ -57,16 +49,6 @@ var Explore = React.createClass({
     });
   },
 
-  showModalTransition(transition) {
-    transition('opacity', {duration: 200, begin: 0, end: 1});
-    transition('height', {duration: 200, begin: DeviceHeight * 2, end: DeviceHeight});
-  },
-
-  hideModalTransition(transition) {
-    transition('height', {duration: 200, begin: DeviceHeight, end: DeviceHeight * 2, reset: true});
-    transition('opacity', {duration: 200, begin: 1, end: 0});
-  },
-
   render(){
     StatusBarIOS.setStyle(StatusBarIOS.Style.darkContent);
     return (
@@ -79,26 +61,9 @@ var Explore = React.createClass({
               selectedIndex={this.state.selectedIndex}
               onValueChange={this._onValueChange} />
           </View>
-          <TouchableOpacity onPress={() => this.openModal()} style={styles.infoButton}>
-            <Icon
-              name='ion|ios-help'
-              size={25}
-              color='#712FA9'
-              style={styles.infoIcon} />
-          </TouchableOpacity>
         </View>
         {this.state.value == 'Picks' && this.renderPicks()}
         {this.state.value == 'Search' && this.renderSearch()}
-        <Modal
-          backdropType="blur"
-          backdropBlur="dark"
-          isVisible={this.state.isModalOpen}
-          forceToFront={true}
-          customShowHandler={this.showModalTransition}
-          customHideHandler={this.hideModalTransition}
-          onPressBackdrop={() => this.closeModal()}>
-          <About />
-        </Modal>
       </View>
     )
   }
@@ -116,20 +81,10 @@ var styles = StyleSheet.create({
     marginBottom: 10
   },
   segmentContainer: {
-    flex: 2,
+    flex: 1,
     marginRight: 10,
     marginLeft: 10,
     marginTop: 4,
-  },
-  infoButton: {
-    flex: 1,
-  },
-  infoIcon: {
-    alignSelf: 'center',
-    width: 35,
-    height: 35,
-    marginLeft: 10,
-    marginRight: 17,
   },
 });
 
