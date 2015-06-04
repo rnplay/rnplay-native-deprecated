31fc534bb2039563f1ea554c15938c4296241942
Johannes Lumpe <johannes@johanneslumpe.de>
2015-06-04 22:45:19 +0300

    Expose RN_VERSION as global

--- App/Utilities/generateAppURL.js
+++ App/Utilities/generateAppURL.js
@@ -1,11 +1,3 @@
-var RN_VERSION = require('../../package.json').dependencies['react-native'];
-var githubPrefix = 'facebook/react-native#';
-if (RN_VERSION.indexOf(githubPrefix) === 0) {
-  RN_VERSION = RN_VERSION.replace(githubPrefix, '');
-} else {
-  RN_VERSION = RN_VERSION.replace(/\./g,'').replace(/-/g, '')
-}
-
 module.exports = (app) => {
-  return `http://packager${RN_VERSION}.rnplay.org/${app.url_token}.bundle`;
+  return `http://packager${global.RN_VERSION}.rnplay.org/${app.url_token}.bundle`;
 };
