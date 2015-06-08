c1c2aba5c340dd816f373f82b22b66e051069e6d
Joshua Sierles <joshua@diluvia.net>
2015-06-09 01:44:52 +0200

    use the jsbundle for release

--- iOS/AppDelegate.m
+++ iOS/AppDelegate.m
@@ -70,8 +70,8 @@ static GAILogLevel const kGANLogLevel = kGAILogLevelWarning;
     initialJSBundleURL = [NSURL URLWithString:[[[NSBundle mainBundle] URLForResource:@"uiexplorer" withExtension:@"jsbundle"] absoluteString]];
     initialModuleName = @"UIExplorerApp";
   } else {
-    /* initialJSBundleURL = [NSURL URLWithString:[[[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"] absoluteString]]; */
-    initialJSBundleURL = [NSURL URLWithString:JSURL];
+    initialJSBundleURL = [NSURL URLWithString:[[[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"] absoluteString]];
+    //initialJSBundleURL = [NSURL URLWithString:JSURL];
     initialModuleName = @"RNPlayNative";
   }
 
