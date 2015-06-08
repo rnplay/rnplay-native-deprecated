532d797688a9264299d3b8fd9136d7b4258e421b
Dave Sibiski <dsibiski@gmail.com>
2015-06-08 16:06:04 -0400

    Adds logo & Title to About screen

--- App/Screens/About.js
+++ App/Screens/About.js
@@ -11,6 +11,7 @@ var {
   StatusBarIOS,
   LinkingIOS,
   TouchableOpacity,
+  Image,
 } = React;
 
 var Help = React.createClass({
@@ -28,33 +29,37 @@ var Help = React.createClass({
       <View style={{flex: 1}}>
         <NavigationBar title={'About'} />
         <ScrollView style={styles.contentContainer}>
-            <Text style={styles.firstText}>Run React Native apps from <TouchableOpacity onPress={() => this._loadWebsite('https://rnplay.org')}><Text style={styles.link}>rnplay.org</Text></TouchableOpacity> directly on your device.</Text>
-            <Text style={styles.aboutText}>Get started by trying apps on the <Text style={styles.emphasis}>Explore</Text> tab, or login to your rnplay.org account in <Text style={styles.emphasis}>My Apps</Text>.</Text>
-            <View style={styles.separator} />
-            <Text style={styles.aboutTitle}>How do I exit a loaded app?</Text>
-            <Text style={styles.aboutText}>Tap and hold the screen with two fingers simultaneously for 1.5 seconds.</Text>
-            <View style={styles.separator} />
-            <Text style={styles.aboutTitle}>What does <Text style={styles.emphasis}>target</Text> mean?</Text>
-            <Text style={styles.aboutText}>The target React Native version for an app. An app might not behave if the target is different than the our bundled React Native version.</Text>
-            <View style={styles.separator} />
-            <Text style={styles.aboutTitle} numberOfLines={2}>
-              What React Native version is this app built with?
-            </Text>
-            <Text style={styles.aboutText}>
-              This app's React Native version is
-              <Text style={styles.buildVersionText}> 0.5.0</Text>, released Friday, June 5th, 2015. We will regularly update this app, tracking React Native.
-            </Text>
-            <View style={styles.separator} />
-            <View style={styles.otherQuestions}>
-              <Text style={styles.otherQuestionsText}>
-                More questions? Pleaes get in touch with us by email. <TouchableOpacity onPress={this._sendEmail}>
-                  <Text style={styles.sendEmailText}>info@rnplay.org</Text>
-                </TouchableOpacity>
+          <Text style={styles.titleText}>React Native Playground</Text>
+          <View style={styles.logoContainer}>
+            <Image style={styles.logo} source={require('image!rnplay_logo_light_w_sand')} />
+          </View>
+          <Text style={styles.firstText}>Run React Native apps from <TouchableOpacity onPress={() => this._loadWebsite('https://rnplay.org')}><Text style={styles.link}>rnplay.org</Text></TouchableOpacity> directly on your device.</Text>
+          <Text style={styles.aboutText}>Get started by trying apps on the <Text style={styles.emphasis}>Explore</Text> tab, or login to your rnplay.org account in <Text style={styles.emphasis}>My Apps</Text>.</Text>
+          <View style={styles.separator} />
+          <Text style={styles.aboutTitle}>How do I exit a loaded app?</Text>
+          <Text style={styles.aboutText}>Tap and hold the screen with two fingers simultaneously for 1.5 seconds.</Text>
+          <View style={styles.separator} />
+          <Text style={styles.aboutTitle}>What does <Text style={styles.emphasis}>target</Text> mean?</Text>
+          <Text style={styles.aboutText}>The target React Native version for an app. An app might not behave if the target is different than the our bundled React Native version.</Text>
+          <View style={styles.separator} />
+          <Text style={styles.aboutTitle} numberOfLines={2}>
+            What React Native version is this app built with?
+          </Text>
+          <Text style={styles.aboutText} numberOfLines={2}>
+            This app is currently built against React Native
+            <Text style={styles.buildVersionText}> 0.5.0</Text>, released Friday, June 5th, 2015.
+          </Text>
+          <View style={styles.separator} />
+          <View style={styles.otherQuestions}>
+            <Text style={styles.otherQuestionsText}>
+              More questions? Pleaes get in touch with us by email. <TouchableOpacity onPress={this._sendEmail}>
+                <Text style={styles.sendEmailText}>info@rnplay.org</Text>
+              </TouchableOpacity>
 
-              </Text>
-              <View style={{paddingTop: 5}}>
-                            </View>
+            </Text>
+            <View style={{paddingTop: 5}}>
             </View>
+          </View>
         </ScrollView>
       </View>
     );
@@ -75,17 +80,33 @@ var styles = StyleSheet.create({
   link: {
     color: "#712FA9"
   },
-
   emphasis: {
     fontStyle: "italic"
   },
-
+  logoContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  logo: {
+    height: 150,
+    width: 150,
+    marginBottom: 10,
+  },
+  titleText: {
+    fontFamily: 'Avenir Next',
+    paddingRight: 15,
+    paddingLeft: 15,
+    marginBottom: 10,
+    fontSize: 24,
+    textAlign: 'center',
+  },
   firstText: {
+    fontFamily: 'Avenir Next',
     paddingRight: 15,
     paddingLeft: 15,
     marginBottom: 20,
-    fontSize: 19,
-    textAlign: 'left',
+    fontSize: 20,
+    textAlign: 'center',
   },
   aboutTitle: {
     fontSize: 18,
@@ -102,7 +123,7 @@ var styles = StyleSheet.create({
     fontSize: 15,
     textAlign: 'left',
     color: '#3a3638',
-    opacity: 0.8,
+    opacity: 0.5,
   },
   separator: {
     flex: 1,
