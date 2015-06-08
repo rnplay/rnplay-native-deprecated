cc6012e7f17e4fcc0c25b00a31d6955ed0ce3b0d
Christopher <hello@blick-labs.com>
2015-06-08 12:15:26 -0700

    add option for sign out

--- App/Screens/MyApps.js
+++ App/Screens/MyApps.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var React = require('react-native');
+var Api = require('../Api/Core');
+var AppActions = require('../Actions/AppActions');
 
 var {
   TouchableOpacity,
@@ -12,12 +14,22 @@ var AppList = require("../Components/AppList");
 var NavigationBar = require('../Components/NavigationBar');
 
 var MyApps = React.createClass({
+
+  _signOut() {
+    Api.delete('/users/sign_out');
+    AppActions.deleteProfile();
+    this.props.navigator.replace({ id: 'login'});
+  },
+
   render() {
     StatusBarIOS.setStyle(StatusBarIOS.Style.lightContent);
 
     return (
       <View style={{flex: 1}}>
-        <NavigationBar title={'My Apps'} />
+        <NavigationBar 
+          title={'My Apps'} 
+          nextTitle={'Sign Out'}
+          onNext={this._signOut}/>
         <AppList url="/apps.json" hideCreator={true} />
       </View>
     )
