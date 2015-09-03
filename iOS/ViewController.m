44199bfee01efaa164e8da9225a4b205de2e179c
Joshua Sierles <joshua@diluvia.net>
2015-09-03 17:22:29 +0200

    fix method name

--- iOS/ViewController.m
+++ iOS/ViewController.m
@@ -19,25 +19,25 @@
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:JSBundleURL
                                                       moduleName:moduleName
                                                    launchOptions:nil];
-  
+
   if (appName == nil) {
     appName = @"app";
   }
-  
+
   NSString *loadingText = [NSString stringWithFormat:@"Loading %@...", appName];
   AppLoadingView *appLoadingView = [[AppLoadingView alloc] initWithLoadingText:loadingText];
-  
+
   rootView.loadingView = appLoadingView;
   rootView.loadingViewFadeDelay = 0.0;
   rootView.loadingViewFadeDuration = 0.15;
   rootView.frame = self.view.bounds;
-  
+
   [self setView:rootView];
-  
-  [self trackScreeView];
+
+  [self trackScreenView];
 }
 
-- (void)trackScreeView {
+- (void)trackScreenView {
   id tracker = [[GAI sharedInstance] defaultTracker];
   [tracker set:kGAIScreenName value:@"App"];
   [tracker send:[[GAIDictionaryBuilder createScreenView] build]];
