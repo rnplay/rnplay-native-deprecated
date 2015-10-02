6bac5c4a6113911c5554209bf4e63c44d7efcb72
Dave Sibiski <dsibiski@gmail.com>
2015-10-02 13:31:18 -0400

    [Android] Extracts reload code out of the native module
    - The AppReloadManager is a class that even the native side can use to load apps.

--- android/app/src/main/java/com/rnplaynative/AppReloaderModule.java
+++ android/app/src/main/java/com/rnplaynative/AppReloaderModule.java
@@ -1,10 +1,5 @@
 package com.rnplaynative;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
-import android.util.Log;
-
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactRootView;
 import com.facebook.react.bridge.NativeModule;
@@ -19,9 +14,6 @@ import java.io.File;
  */
 
 public class AppReloaderModule extends ReactContextBaseJavaModule {
-
-    private static final String PREFS_DEBUG_SERVER_HOST_KEY = "debug_http_host";
-    private static final String JS_BUNDLE_FILE_NAME = "ReactNativeDevBundle.js";
 
     ReactApplicationContext reactContext;
 
@@ -44,26 +36,7 @@ public class AppReloaderModule extends ReactContextBaseJavaModule {
                                         final String moduleName,
                                         final String appName) {
 
-        // Set the 'debug_http_host' to the 'packagerRoot' that was passed in.
-        SharedPreferences mPreferences = PreferenceManager.getDefaultSharedPreferences(reactContext);
-        SharedPreferences.Editor editor = mPreferences.edit();
-        editor.putString(PREFS_DEBUG_SERVER_HOST_KEY, bundleUrl);
-        editor.commit();
-
-        // Delete the jsBundle file for the previously loaded app.
-        File nJSBundleTempFile = new File(reactContext.getFilesDir(), JS_BUNDLE_FILE_NAME);
-        nJSBundleTempFile.delete();
-
-        Log.d("RNPlayNative", "bundleUrl: " + bundleUrl + " bundlePath: " + bundlePath + " moduleName: " + moduleName + " appName: " + appName);
-
-        loadApp(bundlePath, moduleName);
-    }
-
-    private void loadApp(final String bundlePath, final String moduleName) {
-        Intent appActivity = new Intent(reactContext, AppActivity.class);
-        appActivity.putExtra("jsMainModuleName", bundlePath);
-        appActivity.putExtra("moduleName", moduleName);
-        appActivity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        reactContext.startActivity(appActivity);
+        AppReloadManager mAppReloadManager = new AppReloadManager(reactContext);
+        mAppReloadManager.loadApp(bundleUrl, bundlePath, moduleName, appName);
     }
 }
