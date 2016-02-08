4aaaa5e07762a70f0cbfff41bdce8ac63eb289ee
Joshua Sierles <joshua@diluvia.net>
2016-02-08 12:33:54 +0700

    syntax fixes

--- index.ios.js
+++ index.ios.js
@@ -1,14 +1,14 @@
 'use strict';
 
-import React, { AppRegistry, Component } from 'react-native'
-import ApplicationBase from './App/ApplicationBase'
+import React, { AppRegistry, Component } from 'react-native';
+import ApplicationBase from './App/ApplicationBase';
 
 class RNPlayNative extends Component {
   render() {
     return (
       <ApplicationBase />
-    )
+    );
   }
 }
 
-AppRegistry.registerComponent('RNPlayNative', () => RNPlayNative)
+AppRegistry.registerComponent('RNPlayNative', () => RNPlayNative);
